5e18c7a1043f0ae68e8ee21bc451b5f58cd5f0fc
grangabyk-debug <grangabyk@gmail.com>
2026-08-11 05:38:15 -0300

    Add dedicated redesign access page

---
+++ app/redesign/access/page.tsx
@@ -0,0 +1,10 @@
+import AccessClient from './AccessClient'
+
+export const metadata = {
+  title: 'Comercio Lleno · Acceso Rediseño V2',
+  robots: { index: false, follow: false },
+}
+
+export default function RedesignAccessPage() {
+  return <AccessClient />
+}
