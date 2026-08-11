e42dab352fce2fac09adecf977637b8ae0191426
grangabyk-debug <grangabyk@gmail.com>
2026-08-11 04:50:23 -0300

    Add isolated redesign route

---
+++ app/redesign/page.tsx
@@ -0,0 +1,10 @@
+import CommerceApp from './CommerceApp'
+
+export const metadata = {
+  title: 'Comercio Lleno · Rediseño POS',
+  robots: { index: false, follow: false },
+}
+
+export default function RedesignPage() {
+  return <CommerceApp />
+}
