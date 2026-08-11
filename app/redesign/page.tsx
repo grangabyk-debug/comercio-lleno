939ce9aced1e94f2297babe337152734c2071b69
grangabyk-debug <grangabyk@gmail.com>
2026-08-11 17:24:47 -0300

    Activate onboarding for new trial owners

--- app/redesign/page.tsx
+++ app/redesign/page.tsx
@@ -1,5 +1,6 @@
 import CommerceApp from './CommerceApp'
 import TrialStatus from './TrialStatus'
+import OnboardingGuide from './OnboardingGuide'
 import RedesignBrandOverlay from './RedesignBrandOverlay'
 import HeaderRefreshBehavior from './HeaderRefreshBehavior'
 import MobileResponsiveFix from './MobileResponsiveFix'
@@ -14,5 +15,5 @@ export const dynamic = 'force-dynamic'
 export default function RedesignPage() {
   const sha = process.env.VERCEL_GIT_COMMIT_SHA || process.env.GIT_COMMIT_SHA || 'local'
   const buildVersion = sha === 'local' ? 'local' : sha.slice(0, 8)
-  return <><HeaderRefreshBehavior/><MobileResponsiveFix/><RedesignBrandOverlay/><TrialStatus/><CommerceApp buildVersion={buildVersion}/></>
+  return <><HeaderRefreshBehavior/><MobileResponsiveFix/><RedesignBrandOverlay/><TrialStatus/><OnboardingGuide/><CommerceApp buildVersion={buildVersion}/></>
 }
