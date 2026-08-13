ad8de82c38b71cc8f6a2fb9b78feeeb825b128a7
grangabyk-debug <grangabyk@gmail.com>
2026-08-12 22:10:59 -0300

    route promotions to enhanced discount manager

--- app/redesign/ManagementViews.tsx
+++ app/redesign/ManagementViews.tsx
@@ -1,10 +1,10 @@
 export {
   AccountsV2,
   ProfitabilityV2,
-  PromotionsV2,
   ReturnsV2,
   SuppliersV2,
 } from './ManagementViewsLegacy'
 export { default as ProductsV2 } from './ProductsInventory'
+export { default as PromotionsV2 } from './PromotionsEnhanced'
 export { default as SettingsV2 } from './SettingsTenant'
 export { default as PurchasesV2 } from './PurchasesDocuments'
