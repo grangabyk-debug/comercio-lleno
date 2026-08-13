73852ab6f883af48aaa1f33f03ddd32a17c93777
grangabyk-debug <grangabyk@gmail.com>
2026-08-13 07:28:16 -0300

    Enforce subscription gate in mobile app

--- app/movil/page.tsx
+++ app/movil/page.tsx
@@ -5,7 +5,8 @@ import MobileAiAssistant from './MobileAiAssistant'
 import MobilePcNotice from './MobilePcNotice'
 import SaleSearchGuard from './SaleSearchGuard'
 import SessionFetchGuard from '../redesign/SessionFetchGuard'
+import SubscriptionGate from '../redesign/SubscriptionGate'
 
 export const metadata: Metadata={title:'Comercio Lleno · Móvil',description:'Experiencia simple de Comercio Lleno para vender y consultar productos desde el celular',robots:{index:false,follow:false}}
 export const dynamic='force-dynamic'
-export default function MobilePage(){return <><SessionFetchGuard/><SaleSearchGuard/><MobileSimpleApp/><MobileScanner/><MobileAiAssistant/><MobilePcNotice/></>}
+export default function MobilePage(){return <><SessionFetchGuard/><SaleSearchGuard/><SubscriptionGate/><MobileSimpleApp/><MobileScanner/><MobileAiAssistant/><MobilePcNotice/></>}
