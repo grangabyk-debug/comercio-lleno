cc83ddf1fbd3d63817931f84c92e2c3b786d7345
grangabyk-debug <grangabyk@gmail.com>
2026-08-12 22:24:53 -0300

    hide internal permission helper from authenticated rpc

---
+++ supabase/migrations/20260813011300_hide_permission_helper_rpc.sql
@@ -0,0 +1,2 @@
+revoke execute on function public.comercio_has_permission(text, boolean) from authenticated;
+grant execute on function public.comercio_has_permission(text, boolean) to service_role;
