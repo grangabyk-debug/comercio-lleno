8a65e19689517047ed38b9cb5ed31a63f6e23a6a
grangabyk-debug <grangabyk@gmail.com>
2026-08-15 01:41:31 -0300

    Integrar nueva configuración móvil en la app

--- app/movil/page.tsx
+++ app/movil/page.tsx
@@ -4,10 +4,11 @@ import MobileScanner from './MobileScanner'
 import MobileAiAssistant from './MobileAiAssistant'
 import MobilePcNotice from './MobilePcNotice'
 import MobileCashModeController from './MobileCashModeController'
+import MobileSettingsOverlay from './MobileSettingsOverlay'
 import SaleSearchGuard from './SaleSearchGuard'
 import SessionFetchGuard from '../redesign/SessionFetchGuard'
 import SubscriptionGate from '../redesign/SubscriptionGate'
 
 export const metadata: Metadata={title:'Comercio Lleno · Móvil',description:'Experiencia simple de Comercio Lleno para vender y consultar productos desde el celular',robots:{index:false,follow:false}}
 export const dynamic='force-dynamic'
-export default function MobilePage(){return <><SessionFetchGuard/><SaleSearchGuard/><SubscriptionGate/><MobileSimpleApp/><MobileCashModeController/><MobileScanner/><MobileAiAssistant/><MobilePcNotice/></>}
+export default function MobilePage(){return <><SessionFetchGuard/><SaleSearchGuard/><SubscriptionGate/><MobileSimpleApp/><MobileCashModeController/><MobileScanner/><MobileAiAssistant/><MobilePcNotice/><MobileSettingsOverlay/></>}
