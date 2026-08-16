dce1ad99f9a5fda8b6b1907a3eda7c036f474e54
grangabyk-debug <grangabyk@gmail.com>
2026-08-16 17:07:21 -0300

    Integrar permisos en experiencia móvil

--- app/movil/page.tsx
+++ app/movil/page.tsx
@@ -11,6 +11,7 @@ import MobileDarkTheme from './MobileDarkTheme'
 import MobileBrandHeaderOverride from './MobileBrandHeaderOverride'
 import MobileGoogleAccess from './MobileGoogleAccess'
 import MobileArcaStatus from './MobileArcaStatus'
+import MobilePermissionsGate from './MobilePermissionsGate'
 import SaleSearchGuard from './SaleSearchGuard'
 import SessionFetchGuard from '../redesign/SessionFetchGuard'
 import SubscriptionGate from '../redesign/SubscriptionGate'
@@ -18,4 +19,4 @@ import './mobile-modern-v4.css'
 
 export const metadata: Metadata={title:'Comercio Lleno · Móvil',description:'Experiencia simple de Comercio Lleno para vender y consultar productos desde el celular',robots:{index:false,follow:false}}
 export const dynamic='force-dynamic'
-export default function MobilePage(){return <><MobileRevolutionTheme/><MobileDarkTheme/><SessionFetchGuard/><SaleSearchGuard/><SubscriptionGate/><MobileSimpleApp/><MobileArcaStatus/><MobileGoogleAccess/><MobileBrandHeaderOverride/><MobileCashModeController/><MobileScanner/><MobileAiAssistant/><MobilePcNotice/><MobileSettingsOverlay/><MobileLegalLinks/></>}
+export default function MobilePage(){return <><MobileRevolutionTheme/><MobileDarkTheme/><SessionFetchGuard/><SaleSearchGuard/><SubscriptionGate/><MobileSimpleApp/><MobileArcaStatus/><MobileGoogleAccess/><MobileBrandHeaderOverride/><MobileCashModeController/><MobileScanner/><MobileAiAssistant/><MobilePcNotice/><MobileSettingsOverlay/><MobileLegalLinks/><MobilePermissionsGate/></>}
