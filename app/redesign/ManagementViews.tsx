fee64f670b21335450240b9b6908b1e994b3d824
grangabyk-debug <grangabyk@gmail.com>
2026-08-17 15:16:53 -0300

    Enable cash discount promotions and stock refresh fixes

--- app/redesign/ManagementViews.tsx
+++ app/redesign/ManagementViews.tsx
@@ -4,7 +4,7 @@ export {
   ReturnsV2,
   SuppliersV2,
 } from './ManagementViewsLegacy'
-export { default as ProductsV2 } from './ProductsInventory'
-export { default as PromotionsV2 } from './PromotionsEnhanced'
+export { default as ProductsV2 } from './ProductsInventoryFixed'
+export { default as PromotionsV2 } from './PromotionsWithCashDiscount'
 export { default as SettingsV2 } from './SettingsWithMobile'
 export { default as PurchasesV2 } from './PurchasesOwnerDelete'
