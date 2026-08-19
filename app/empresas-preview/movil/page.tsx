07daaf787196a4154c759ec5444cf7605a2b079b
grangabyk-debug <grangabyk@gmail.com>
2026-08-19 20:16:41 -0300

    Add employer mobile assistant page

---
+++ app/empresas-preview/movil/page.tsx
@@ -0,0 +1,6 @@
+import EmployerPocket from './EmployerPocket'
+import '../../postula-preview/premium-v3.css'
+
+export const metadata={title:'Modo móvil | Postulá Mejor Empresas',description:'Asistente móvil de selección para dueños y responsables.',robots:{index:false,follow:false}}
+
+export default function EmployerMobilePage(){return <EmployerPocket/>}
