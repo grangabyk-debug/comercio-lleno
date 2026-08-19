21e4f3cc66f27183e4f18c6ae2e25d033674b255
grangabyk-debug <grangabyk@gmail.com>
2026-08-19 20:51:53 -0300

    Apply V6 legal footer styling to job detail

--- app/empleos-preview/[slug]/page.tsx
+++ app/empleos-preview/[slug]/page.tsx
@@ -4,6 +4,7 @@ import styles from '../../postula-preview/platform.module.css'
 import {PlatformHeader,PlatformFooter,MobileNav} from '../../postula-preview/PlatformChrome'
 import {getJobCatalog} from '../../postula-preview/jobs'
 import '../../postula-preview/premium-v5.css'
+import '../../postula-preview/premium-v6.css'
 import '../../postula-preview/inner-v5.css'
 
 export const revalidate=21600
