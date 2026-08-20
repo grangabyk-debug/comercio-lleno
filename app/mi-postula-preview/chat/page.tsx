1db86c3fc72bea17ce0afa9492cc538deea33940
grangabyk-debug <grangabyk@gmail.com>
2026-08-19 22:36:28 -0300

    Add full-screen candidate chat page

---
+++ app/mi-postula-preview/chat/page.tsx
@@ -0,0 +1,6 @@
+import CandidateChat from './CandidateChat'
+import '../../postula-preview/premium-v7.css'
+import './chat.css'
+
+export const metadata={title:'Mensajes | Postulá Mejor'}
+export default function CandidateChatPage(){return <CandidateChat/>}
