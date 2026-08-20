49343b24d1ba414c4f9ea70626407b75324db325
grangabyk-debug <grangabyk@gmail.com>
2026-08-19 21:03:38 -0300

    Apply V7 employer chrome to registration

--- app/empresas-preview/registro/page.tsx
+++ app/empresas-preview/registro/page.tsx
@@ -2,5 +2,6 @@ import styles from '../../postula-preview/platform.module.css'
 import {PlatformHeader,PlatformFooter} from '../../postula-preview/PlatformChrome'
 import EmployerRegistration from './EmployerRegistration'
 import '../../postula-preview/premium-v6.css'
+import '../../postula-preview/premium-v7.css'
 export const metadata={title:'Crear empresa | Postulá Mejor Preview',robots:{index:false,follow:false}}
-export default function EmployerRegister(){return <main className={styles.page}><PlatformHeader audience="employer"/><EmployerRegistration/><PlatformFooter/></main>}
+export default function EmployerRegister(){return <main className={`${styles.page} pm7-page pm7-employer`}><PlatformHeader audience="employer"/><EmployerRegistration/><PlatformFooter/></main>}
