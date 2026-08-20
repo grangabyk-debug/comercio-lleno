f17443ca0cf89df4cabf40bc084c5397ca37f82e
grangabyk-debug <grangabyk@gmail.com>
2026-08-19 23:32:24 -0300

    Apply Postula metadata to access

---
+++ app/postula-acceso-preview/layout.tsx
@@ -0,0 +1,3 @@
+import {postulaProductMetadata} from '../postulaProductMetadata'
+export const metadata=postulaProductMetadata
+export default function Layout({children}:{children:React.ReactNode}){return children}
