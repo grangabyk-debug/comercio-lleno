e85bc1974f58e8e5a2a71804b9444e495697e77e
grangabyk-debug <grangabyk@gmail.com>
2026-08-21 20:25:05 -0300

    Postulá Mejor: cargar estilo de aviso confidencial

--- app/empresas-preview/publicar/page.tsx
+++ app/empresas-preview/publicar/page.tsx
@@ -3,5 +3,6 @@ import {PlatformHeader,PlatformFooter} from '../../postula-preview/PlatformChrom
 import EmployerPublishWizard from './EmployerPublishWizard'
 import '../../postula-preview/premium-v6.css'
 import '../../postula-preview/premium-v7.css'
+import './employer-confidential-v21.css'
 export const metadata={title:{absolute:'Publicar búsqueda | Postulá Mejor Empresas'},robots:{index:false,follow:false},alternates:{canonical:'https://postulamejor.com/empresas/publicar'}}
 export default function PublishPage(){return <main className={`${styles.page} pm7-page pm7-employer`}><PlatformHeader audience="employer"/><div className={styles.wizard}><EmployerPublishWizard/></div><PlatformFooter/></main>}
