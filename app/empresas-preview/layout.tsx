6e5b53e02cc691fd747ba9527ab448ede239b814
grangabyk-debug <grangabyk@gmail.com>
2026-08-24 13:49:57 -0300

    Postula empresas: rediseñar header mobile

--- app/empresas-preview/layout.tsx
+++ app/empresas-preview/layout.tsx
@@ -6,5 +6,6 @@ import './employer-plans-v19.css'
 import './employer-access-v20.css'
 import './employer-stability-v21.css'
 import './employer-header-v22.css'
+import './employer-header-v39.css'
 export const metadata=postulaProductMetadata
 export default function Layout({children}:{children:React.ReactNode}){return <>{children}<EmployerPlansUpgrade/><FlexPlanBenefitsBridge/></>}
