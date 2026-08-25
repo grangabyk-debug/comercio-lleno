af51f97b200c8f1ec427c20b2c79891d4ce5fa35
grangabyk-debug <grangabyk@gmail.com>
2026-08-25 18:43:24 -0300

    Postula: aplicar nombre Servicios Flex en mensajes

--- app/mi-postula-preview/chat/page.tsx
+++ app/mi-postula-preview/chat/page.tsx
@@ -1,6 +1,7 @@
 import CandidateChat from './CandidateChat'
+import FlexNamingBridge from '../../postula-preview/FlexNamingBridge'
 import '../../postula-preview/premium-v7.css'
 import './chat.css'
 
 export const metadata={title:'Mensajes | Postulá Mejor'}
-export default function CandidateChatPage(){return <CandidateChat/>}
+export default function CandidateChatPage(){return <><FlexNamingBridge/><CandidateChat/></>}
