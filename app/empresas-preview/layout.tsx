3cf111d0470b22d0313b6b348fbe918ed406e7a8
grangabyk-debug <grangabyk@gmail.com>
2026-08-26 19:57:39 -0300

    Postula: aplicar recorte de landing empresas

--- app/empresas-preview/layout.tsx
+++ app/empresas-preview/layout.tsx
@@ -7,5 +7,6 @@ import './employer-access-v20.css'
 import './employer-stability-v21.css'
 import './employer-header.css'
 import './employer-minimal-v46.css'
+import './employer-trim-v47.css'
 export const metadata=postulaProductMetadata
 export default function Layout({children}:{children:React.ReactNode}){return <>{children}<EmployerPlansUpgrade/><FlexPlanBenefitsBridge/></>}
