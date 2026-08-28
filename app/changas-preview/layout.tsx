b7878609ad710ee7feaf1714cbe16a1cf9d921fa
grangabyk-debug <grangabyk@gmail.com>
2026-08-28 20:36:09 -0300

    mount Flex consent privacy polish

--- app/changas-preview/layout.tsx
+++ app/changas-preview/layout.tsx
@@ -1,3 +1,4 @@
 import {postulaProductMetadata} from '../postulaProductMetadata'
+import ServicesFlexConsentPolish from './ServicesFlexConsentPolish'
 export const metadata=postulaProductMetadata
-export default function Layout({children}:{children:React.ReactNode}){return children}
+export default function Layout({children}:{children:React.ReactNode}){return <>{children}<ServicesFlexConsentPolish/></>}
