47d293f80a646311ce53950aa65b25462c05ae63
grangabyk-debug <grangabyk@gmail.com>
2026-08-28 13:18:35 -0300

    style(postula): load landing-style chat theme

--- app/mi-postula-preview/chat/page.tsx
+++ app/mi-postula-preview/chat/page.tsx
@@ -2,6 +2,7 @@ import CandidateChat from './CandidateChat'
 import FlexNamingBridge from '../../postula-preview/FlexNamingBridge'
 import '../../postula-preview/premium-v7.css'
 import './chat.css'
+import './chat-landing-v54.css'
 
 export const metadata={title:'Mensajes | Postulá Mejor'}
 export default function CandidateChatPage(){return <><FlexNamingBridge/><CandidateChat/></>}
