6c077745c792f994594c66b224b59ce6f822e94a
grangabyk-debug <grangabyk@gmail.com>
2026-08-31 19:24:38 -0300

    fix: remove final retired support dependencies

--- app/soporte-admin/page.tsx
+++
@@ -1,6 +0,0 @@
-import type {Metadata} from 'next'
-import SupportAdminClient from './SupportAdminClient'
-import './support-admin.css'
-
-export const metadata:Metadata={title:{absolute:'Soporte | Postulá Mejor'},robots:{index:false,follow:false}}
-export default function SupportAdminPage(){return <SupportAdminClient/>}
